dbe8917fff99fe796c12c77a049b60fefee619c2
沈文兴 <bjshenwenxing@corp.netease.com>
2019-03-04 17:08:27 +0800

    ADD: 最基础的OpenGL相关函数调用

---
+++ app/src/main/java/com/swx/opengldemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.swx.opengldemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import com.swx.opengldemo.databinding.ActivityMainBinding;
+import com.swx.opengldemo.render.ShapeRender;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main );
+        glSurfaceView = mBinding.glView;
+        GLSurfaceView.Renderer render = new ShapeRender();
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(render);
+    }
+
+}
